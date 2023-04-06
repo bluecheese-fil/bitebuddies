eeef6d098425dc73d78f6330bc888005ed01e367
Filippo Guerra <filippoguerra2001@gmail.com>
2023-04-06 11:13:28 +0200

    Fixed login and signup
    - Signup still not centered
    (This will be merged with main)

--- js/signup.js
+++ js/signup.js
@@ -3,13 +3,13 @@ function loadLoginIntoSignup(){
   if(saved_email == null || saved_email == "null") return false;
 
   document.getElementById("email_1").value = saved_email;
-  sessionStorage.setItem("saved_email", "null")
+  sessionStorage.setItem("saved_email", null)
 
   saved_passwd = sessionStorage.getItem("saved_password");
-  if(saved_passwd == null || saved_passwd == "null") return ;
+  if(saved_passwd == null || saved_passwd == null) return false;
 
   document.getElementById("password_1").value = saved_passwd;
-  sessionStorage.setItem("saved_password", "null")
+  sessionStorage.setItem("saved_password", null)
 }
 
 // I'm using custom function to mark a particular item as "touched". When that happens, I can say for certain that, if it's still empty, then something must be written there
@@ -18,5 +18,7 @@ function checkText(elem){
   var formText = document.getElementById(elem).value
   if(formText == ""){
     document.getElementById("errore" + elem).removeAttribute('hidden')
+  } else {
+    document.getElementById("errore" + elem).setAttribute('hidden', true)
   }
 }
