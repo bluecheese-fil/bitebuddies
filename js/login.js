040b0f0f165c65faf015e9a2e3240ba9ee814c67
Filippo Guerra <filippoguerra2001@gmail.com>
2023-04-06 17:37:48 +0200

    Fixed login and signup js
    - Added new type of error in login html page
    - Added saving username and password in localStorage in login.js
    - Changed name for sessionstorage elements in redirect to signup.html

--- js/login.js
+++ js/login.js
@@ -1,22 +1,60 @@
-function checkForm(){
-  // email is valid?
-
-  // password is valid?
-}
-
 function accountCreation(){
   email = document.getElementById("email").value;
   passwd = document.getElementById("password").value
-  sessionStorage.setItem("saved_email", email)
-  sessionStorage.setItem("saved_password", passwd)
+  sessionStorage.setItem("signupemail", email)
+  sessionStorage.setItem("signuppasswd", passwd)
+}
+
+function savedLogin(){
+  const loginForm = document.querySelector('.loginform');
+
+  if(localStorage.getItem("user") != null && localStorage.getItem("user") != ""
+    && localStorage.getItem("password") != null && localStorage.getItem("password") != ""){
+    // make query request for login
+
+    if(false){ //login worked
+      window.location.replace("/homepage.html");
+    } else {
+      document.getElementById("passwordchanged").removeAttribute('hidden');
+      
+      loginForm.style.height = "23vh";
+      loginForm.style.minHeight = "180px";
+
+      localStorage.removeItem("email");
+      localStorage.removeItem("password")
+    }
+  }
 }
 
 function tryLogin(){
-  // if either email is not found or password is not correct!
-  document.getElementById("errore").removeAttribute('hidden');
-  document.getElementById("password").value = "";
-
   const loginForm = document.querySelector('.loginform');
-  loginForm.style.height = "26vh";
-  loginForm.style.minHeight = "200px";
+
+  document.getElementById("passwordchanged").setAttribute('hidden', true);
+  loginForm.style.height = "16vh"
+  loginForm.style.minHeight = "140px"
+
+  email = document.getElementById("email").value
+  passwd = document.getElementById("password").value
+  toRemember = document.getElementById("saveaccount").checked
+
+  // make query call to database
+
+  // if found
+  if(false){
+    
+    if(toRemember){
+      localStorage.setItem("user") = email;
+      localStorage.setItem("password") = passwd;
+    }
+
+    window.location.replace("/homepage.html");
+  } else {
+    // if either email is not found or password is not correct!
+    
+    document.getElementById("error").removeAttribute('hidden');
+    document.getElementById("password").value = ""; 
+    
+    loginForm.style.height = "26vh";
+    loginForm.style.minHeight = "200px";
+  }
 }
--- js/signup.js
+++ js/signup.js
@@ -1,15 +1,14 @@
 function loadLoginIntoSignup(){
-  saved_email = sessionStorage.getItem("saved_email");
-  if(saved_email == null || saved_email == "null") return false;
+  saved_email = sessionStorage.getItem("signupemail");
+  saved_passwd = sessionStorage.getItem("signuppasswd");
+  
+  sessionStorage.removeItem("signupemail")
+  sessionStorage.removeItem("signuppasswd")
+  
+  if(saved_email == null || saved_passwd == null) return false;
 
   document.getElementById("email_1").value = saved_email;
-  sessionStorage.setItem("saved_email", null)
-
-  saved_passwd = sessionStorage.getItem("saved_password");
-  if(saved_passwd == null || saved_passwd == null) return false;
-
   document.getElementById("password_1").value = saved_passwd;
-  sessionStorage.setItem("saved_password", null)
 }
 
 // I'm using custom function to mark a particular item as "touched". When that happens, I can say for certain that, if it's still empty, then something must be written there
