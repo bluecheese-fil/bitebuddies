1360684c05a9768a36fb6159d9dd4ee738132dbf
Filippo Guerra <filippoguerra2001@gmail.com>
2023-05-16 23:24:57 +0200

    Fixed database signin in "menu.php"

--- php/menu.php
+++ php/menu.php
@@ -7,7 +7,7 @@
   /*1*/
   function menu() {
     $validtoken1="select * from menù";
-    $db = pg_connect("host=localhost port=5432 dbname=BiteBuddies user=postgres password=patatine1") or die('Could not connect:'.pg_last_error());
+    $db = pg_connect("host=localhost port=5432 dbname=BiteBuddies user=bitebuddies password=bites1!") or die('Could not connect:'.pg_last_error());
     $result1=pg_query($db, $validtoken1) or die('Query failed:'.pg_last_error());
     $data1=array();
     while ($line=pg_fetch_array($result1, null, PGSQL_ASSOC)) {
@@ -21,7 +21,7 @@
   /*2*/
   function change() {
     $validtoken1="select * from menù";
-    $db = pg_connect("host=localhost port=5432 dbname=BiteBuddies user=postgres password=patatine1") or die('Could not connect:'.pg_last_error());
+    $db = pg_connect("host=localhost port=5432 dbname=BiteBuddies user=bitebuddies password=bites1!") or die('Could not connect:'.pg_last_error());
     $result1=pg_query($db, $validtoken1) or die('Query failed:'.pg_last_error());
     $data1=array();
     while ($line=pg_fetch_array($result1, null, PGSQL_ASSOC)) {
@@ -35,7 +35,7 @@
   /*3*/
   function ristoranti() {
     $validtoken2="select * from ristoranti order by rest_id";
-    $db = pg_connect("host=localhost port=5432 dbname=BiteBuddies user=postgres password=patatine1") or die('Could not connect:'.pg_last_error());
+    $db = pg_connect("host=localhost port=5432 dbname=BiteBuddies user=bitebuddies password=bites1!") or die('Could not connect:'.pg_last_error());
     $result2=pg_query($db, $validtoken2) or die('Query failed:'.pg_last_error());
     $data2=array();
     while ($line = pg_fetch_array($result2, null, PGSQL_ASSOC)) {
@@ -48,7 +48,7 @@
 
   function loadQuantita() {
     $validtoken2="select * from menù";
-    $db = pg_connect("host=localhost port=5432 dbname=BiteBuddies user=postgres password=patatine1") or die('Could not connect:'.pg_last_error());
+    $db = pg_connect("host=localhost port=5432 dbname=BiteBuddies user=bitebuddies password=bites1!") or die('Could not connect:'.pg_last_error());
     $result2=pg_query($db, $validtoken2) or die('Query failed:'.pg_last_error());
     $data2=array();
     while ($line = pg_fetch_array($result2, null, PGSQL_ASSOC)) {
