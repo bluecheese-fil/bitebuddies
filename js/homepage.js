e2e99ad6e3f73b70db7818e55d70458edcea4469
Filippo Guerra <filippoguerra2001@gmail.com>
2023-05-22 13:13:16 +0200

    Merged items and minor updates

--- js/homepage.js
+++ js/homepage.js
@@ -28,20 +28,12 @@ function isLogged() {
 }
 
 function loadHomepage(){
-  $('#cerca').on('keydown', function(event) {
-    if(event.keyCode === 13 && !event.shiftKey) {
-      event.preventDefault();
-      var name = $(this).val();
-      window.open(name + ".html", '_blank');
-    }
-  });
-
   conta1=0;
   $("#bottone_sx1").prop("disabled", true);
   $("#bottone_dx1").click(function() {
     var cucina=$(".table1");
     for(var i=0; i<9; i++) {
-      $(cucina[i]).animate({left: "-=20%"}, 500);
+      $(cucina[i]).animate({marginLeft: "-=116%"}, 400);
     }
     conta1++;
     if(conta1==0) {
@@ -58,7 +50,7 @@ function loadHomepage(){
   $("#bottone_sx1").click(function() {
     var cucina=$(".table1");
     for(var i=0; i<9; i++) {
-      $(cucina[i]).animate({left: "+=20%"}, 500);
+      $(cucina[i]).animate({marginLeft: "+=116%"}, 400);
     }
     conta1--;
     if(conta1==0) {
@@ -71,47 +63,6 @@ function loadHomepage(){
     if(conta1==2) {
       $("#bottone_dx1").prop("disabled", true);
     }
-  });
-
-  conta2=0;
-  $("#bottone_sx2").prop("disabled", true);
-  $("#bottone_dx2").click(function() {
-    conta2++;
-    var cucina2=$(".table2");
-    for(var i=0; i<8; i++) {
-      $(cucina2[i]).animate({left: "-=20%"}, 400);
-    }
-    if(conta2==0) {
-      $("#bottone_sx2").prop("disabled", true);
-    }
-    if(conta2>0 && conta2<2) {
-      $("#bottone_sx2").prop("disabled", false);
-      $("#bottone_dx2").prop("disabled", false);
-    }
-    if(conta2==2) {
-      $("#bottone_dx2").prop("disabled", true);
-    }
-  });
-  $("#bottone_sx2").click(function() {
-    var cucina2=$(".table2");
-    for(var i=0; i<8; i++) {
-      $(cucina2[i]).animate({left: "+=20%"}, 400);
-    }
-    conta2--;
-    if(conta2==0) {
-      $("#bottone_sx2").prop("disabled", true);
-    }
-    if(conta2>0 && conta2<2) {
-      $("#bottone_sx2").prop("disabled", false);
-      $("#bottone_dx2").prop("disabled", false);
-    }
-    if(conta2==2) {
-      $("#bottone_dx2").prop("disabled", true);
-    }
-  });
-
-  $("#bottone_basso").click(function() {
-    $('html, body').animate({scrollTop: 679}, 100);
   });
 }
 
@@ -256,81 +207,6 @@ function cercaRistorante() {
 }
 
 function scroll() {
-  $('html, body').animate({scrollTop: 679}, 10);
-}
-
-function loadHomepage(){
-  conta1=0;
-  $("#bottone_sx1").prop("disabled", true);
-  $("#bottone_dx1").click(function() {
-    var cucina=$(".table1");
-    for(var i=0; i<9; i++) {
-      $(cucina[i]).animate({left: "-=20%"}, 500);
-    }
-    conta1++;
-    if(conta1==0) {
-      $("#bottone_sx1").prop("disabled", true);
-    }
-    if(conta1>0 && conta1<2) {
-      $("#bottone_sx1").prop("disabled", false);
-      $("#bottone_dx1").prop("disabled", false);
-    }
-    if(conta1==2) {
-      $("#bottone_dx1").prop("disabled", true);
-    }
-  });
-  $("#bottone_sx1").click(function() {
-    var cucina=$(".table1");
-    for(var i=0; i<9; i++) {
-      $(cucina[i]).animate({left: "+=20%"}, 500);
-    }
-    conta1--;
-    if(conta1==0) {
-      $("#bottone_sx1").prop("disabled", true);
-    }
-    if(conta1>0 && conta1<2) {
-      $("#bottone_sx1").prop("disabled", false);
-      $("#bottone_dx1").prop("disabled", false);
-    }
-    if(conta1==2) {
-      $("#bottone_dx1").prop("disabled", true);
-    }
-  });
-
-  conta2=0;
-  $("#bottone_sx2").prop("disabled", true);
-  $("#bottone_dx2").click(function() {
-    conta2++;
-    var cucina2=$(".table2");
-    for(var i=0; i<8; i++) {
-      $(cucina2[i]).animate({left: "-=20%"}, 400);
-    }
-    if(conta2==0) {
-      $("#bottone_sx2").prop("disabled", true);
-    }
-    if(conta2>0 && conta2<2) {
-      $("#bottone_sx2").prop("disabled", false);
-      $("#bottone_dx2").prop("disabled", false);
-    }
-    if(conta2==2) {
-      $("#bottone_dx2").prop("disabled", true);
-    }
-  });
-  $("#bottone_sx2").click(function() {
-    var cucina2=$(".table2");
-    for(var i=0; i<8; i++) {
-      $(cucina2[i]).animate({left: "+=20%"}, 400);
-    }
-    conta2--;
-    if(conta2==0) {
-      $("#bottone_sx2").prop("disabled", true);
-    }
-    if(conta2>0 && conta2<2) {
-      $("#bottone_sx2").prop("disabled", false);
-      $("#bottone_dx2").prop("disabled", false);
-    }
-    if(conta2==2) {
-      $("#bottone_dx2").prop("disabled", true);
-    }
-  });
+  scrollAmount = Number.parseFloat($(".contenitore").position()["top"]) + Number.parseFloat($(".contenitore").height());
+  $('html, body').animate({scrollTop: scrollAmount}, 10);
 }
