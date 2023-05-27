f114c78ffcaca2b0924ddf78019a912b831b3b3d
Filippo Guerra <filippoguerra2001@gmail.com>
2023-05-27 18:02:08 +0200

    Moved isLOgged into generalFunctions

--- js/categories.js
+++ js/categories.js
@@ -45,32 +45,3 @@ function loadContenuto(file) {
         }, error:function(response){ window.location.href = "/500.html"; }
     });
 }
-
-function isLogged() {
-  let jsoncookie = {};
-  let cookies = document.cookie.split("; ");
-  for(let i = 0; i<cookies.length; i++){
-    let point = cookies[i].split("=");
-    if(point[0] == "iv" || point[0] == "saveduser") jsoncookie[point[0]] = point[1];
-  }
-  
-  if(!jsoncookie.hasOwnProperty("saveduser") || !jsoncookie.hasOwnProperty("iv")){ $("#personallogin").remove(); return ; }
-  
-  $.ajax({
-    url: "/php/homepage.php",
-    type: "POST",
-    dataType: "JSON",
-    data: (jsoncookie),
-    success: function(response){
-      if(response["usrfound"] == 0) $("#personallogin").remove();
-      else {
-        $("#name").text("Ciao " + response["name"]);
-        $("#personallogin").removeAttr("hidden"); $("#fastredirect").remove();
-      }
-    },
-    error: function(){ 
-      console.log("Server error");
-      alert("A causa di un errore del server, al momento non è possibile accedere al proprio account o fare ordini. Ci scusiamo per il disagio");
-    }
-  });
-}
--- js/homepage.js
+++ js/homepage.js
@@ -1,32 +1,3 @@
-function isLogged() {
-  let jsoncookie = {};
-  let cookies = document.cookie.split("; ");
-  for(let i = 0; i<cookies.length; i++){
-    let point = cookies[i].split("=");
-    if(point[0] == "iv" || point[0] == "saveduser") jsoncookie[point[0]] = point[1];
-  }
-
-  if(!jsoncookie.hasOwnProperty("saveduser") || !jsoncookie.hasOwnProperty("iv")){ $("#personallogin").remove(); return ; }
-
-  $.ajax({
-    url: "/php/homepage.php",
-    type: "POST",
-    dataType: "JSON",
-    data: (jsoncookie),
-    success: function(response){
-      if(response["usrfound"] == 0) $("#personallogin").remove();
-      else {
-        $("#name").text("Ciao " + response["name"]);
-        $("#personallogin").removeAttr("hidden"); $("#fastredirect").remove();
-      }
-    },
-    error: function(){ 
-      console.log("Server error");
-      alert("A causa di un errore del server, al momento non è possibile accedere al proprio account o fare ordini. Ci scusiamo per il disagio");
-    }
-  });
-}
-
 function loadHomepage(){
   conta1=0;
   $("#bottone_sx1").prop("disabled", true);
